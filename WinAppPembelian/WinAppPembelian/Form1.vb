508366c3e28c6f1c3cf27ea351518b26b513a9f3
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2019-03-01 08:16:59 +0700

    ok

--- WinAppPembelian/WinAppPembelian/Form1.vb
+++ WinAppPembelian/WinAppPembelian/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
 
     End Sub
 End Class
